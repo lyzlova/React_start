f97a62971c0e0198a1a82cb984c7eae873e939f9
Ivanna <lyzlovaivanna@gmail.com>
2019-05-28 15:03:07 +0300

    add posts

--- insta-react/src/components/Posts.js
+++ insta-react/src/components/Posts.js
@@ -1,15 +1,62 @@
 import React, { Component } from 'react';
-import Post from './Post';
+import InstaService from '../services/instaservice';
+import User from './User';
+import ErrorMessage from './ErrorMessage';
+
+// import Post from './Post';
 
 export default class Posts extends Component {
+  Instaservice = new InstaService();
+  state = {
+    posts: [],
+    error: false,
+  };
+
+  componentDidMount() {
+    this.updatePosts();
+  }
+
+  updatePosts() {
+    this.Instaservice.getAllPosts()
+      .then(this.onPostsLoaded)
+      .catch();
+  }
+
+  onPostsLoaded = posts => {
+    this.setState({
+      posts,
+      error: false,
+    });
+  };
+
+  onError = err => {
+    this.setState({
+      error: true,
+    });
+  };
+
+  renderItems(arr) {
+    return arr.map(item => {
+      const { name, altname, photo, src, alt, descr, id } = item;
+      return (
+        <div key={id} className="post">
+          <User src={photo} alt={altname} name={name} />
+          <img src={src} alt={alt} />
+          <div className="post__name">{name}</div>
+          <div className="post__descr">{descr}</div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return (
-      <div className="left">
-        <Post
-          alt="nature"
-          src="https://st2.depositphotos.com/5371668/8408/i/450/depositphotos_84086784-stock-photo-man-and-the-universe.jpg"
-        />
-      </div>
-    );
+    const { error, posts } = this.state;
+    if (error) {
+      return <ErrorMessage />;
+    }
+
+    const items = this.renderItems(posts);
+
+    return <div className="left">{items}</div>;
   }
 }
--- insta-react/src/components/Users.js
+++ insta-react/src/components/Users.js
@@ -1,40 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import User from './User';
+import InstaService from '../services/instaservice';
+import ErrorMessage from './ErrorMessage';
 
-export default function Users() {
-  return (
-    <div className="right">
-      <User
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuaDMkde2wg-uSiVulXbm0dpCL8m2aMf7eXe38f4BtV3jeXAYYGQ"
-        alt="woman"
-        name="woman"
-      />
-      <div className="users__block">
-        <User
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuaDMkde2wg-uSiVulXbm0dpCL8m2aMf7eXe38f4BtV3jeXAYYGQ"
-          alt="woman"
-          name="woman"
-          min
-        />
-        <User
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuaDMkde2wg-uSiVulXbm0dpCL8m2aMf7eXe38f4BtV3jeXAYYGQ"
-          alt="woman"
-          name="woman"
-          min
-        />
-        <User
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuaDMkde2wg-uSiVulXbm0dpCL8m2aMf7eXe38f4BtV3jeXAYYGQ"
-          alt="woman"
-          name="woman"
-          min
-        />
-        <User
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuaDMkde2wg-uSiVulXbm0dpCL8m2aMf7eXe38f4BtV3jeXAYYGQ"
-          alt="woman"
-          name="woman"
-          min
-        />
-      </div>
-    </div>
-  );
+export default class Posts extends Component {
+  Instaservice = new InstaService();
+  state = {
+    posts: [],
+    error: false,
+  };
+
+  componentDidMount() {
+    this.updatePosts();
+  }
+
+  updatePosts() {
+    this.Instaservice.getAllPosts()
+      .then(this.onPostsLoaded)
+      .catch();
+  }
+
+  onPostsLoaded = posts => {
+    this.setState({
+      posts,
+      error: false,
+    });
+  };
+
+  onError = err => {
+    this.setState({
+      error: true,
+    });
+  };
+
+  renderItems(arr) {
+    return arr.map(item => {
+      const { name, altname, photo } = item;
+      return <User src={photo} alt={altname} name={name} />;
+    });
+  }
+
+  render() {
+    const { error, posts } = this.state;
+    if (error) {
+      return <ErrorMessage />;
+    }
+
+    const items = this.renderItems(posts);
+
+    return (
+      <>
+        <div className="right">
+          <User
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuaDMkde2wg-uSiVulXbm0dpCL8m2aMf7eXe38f4BtV3jeXAYYGQ"
+            alt="woman"
+            name="woman"
+          />
+          <div className="users__block">{items}</div>
+        </div>
+      </>
+    );
+  }
 }
